83d0105b30bb8b281f44c1af45c9b1a340d20ca8
jihwan38 <sss203800@gmail.com>
2025-07-25 16:59:13 +0900

    refactor: UriComponentBuilder 를 사용하여 인가코드를 받는 Uri 설정하도록 변경

--- src/main/java/gift/kakao/LoginPageController.java
+++ src/main/java/gift/kakao/LoginPageController.java
@@ -5,19 +5,29 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.util.UriComponentsBuilder;
 
 
 @Controller
 public class LoginPageController {
     @Value("${kakao.client_id}")
-    private String client_id;
+    private String clientId;
 
     @Value("${kakao.redirect_uri}")
-    private String redirect_uri;
+    private String redirectUri;
+
+    @Value("${kakao.oauth_uri}")
+    private String kakaoOauthUri;
 
     @GetMapping("/kakao/login")
     public String login(Model model){
-        String location = "https://kauth.kakao.com/oauth/authorize?response_type=code&client_id="+client_id+"&redirect_uri="+redirect_uri;
+        String location = UriComponentsBuilder
+                .fromUriString(kakaoOauthUri)
+                .queryParam("response_type", "code")
+                .queryParam("client_id", clientId)
+                .queryParam("redirect_uri", redirectUri)
+                .toUriString();
+
         model.addAttribute("location", location);
 
         return "/kakao/login";
