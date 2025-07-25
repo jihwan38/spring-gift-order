9740ead38ee73eaf52206c1ab03f0601c4c816ef
jihwan38 <sss203800@gmail.com>
2025-07-25 09:00:32 +0900

    feat: @JsonIgnoreProperties(ignoreUnknown = true) AgreeScopeInfo와 KakaoAgreeResponseDto 에 적용

--- src/main/java/gift/kakao/dto/AgreeScopeInfo.java
+++ src/main/java/gift/kakao/dto/AgreeScopeInfo.java
@@ -1,5 +1,8 @@
 package gift.kakao.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public record AgreeScopeInfo(
         String id,
         Boolean agreed
