87ad97561e901af9009e03790933963e11c50194
jihwan38 <sss203800@gmail.com>
2025-07-28 03:02:51 +0900

    refactor: Order 엔티티 들여쓰기 리팩토링

--- src/main/java/gift/order/entity/Order.java
+++ src/main/java/gift/order/entity/Order.java
@@ -9,59 +9,77 @@ import java.time.LocalDateTime;
 @Entity
 @Table(name = "orders")
 public class Order {
-        @Id
-        @GeneratedValue(strategy = GenerationType.IDENTITY)
-        private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-        @ManyToOne(fetch = FetchType.LAZY)
-        @JoinColumn(name = "option_id", nullable = false)
-        private Option option;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "option_id", nullable = false)
+    private Option option;
 
-        @Column(name = "quantity", nullable = false)
-        private int quantity;
+    @Column(name = "quantity", nullable = false)
+    private int quantity;
 
-        @Column(name = "order_date_time", nullable = false)
-        private LocalDateTime orderDateTime;
+    @Column(name = "order_date_time", nullable = false)
+    private LocalDateTime orderDateTime;
 
-        @Column(name = "message", nullable = false)
-        String message;
+    @Column(name = "message", nullable = false)
+    String message;
 
-        @ManyToOne(fetch = FetchType.LAZY)
-        @JoinColumn(name = "member_id", nullable = false)
-        Member member;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", nullable = false)
+    Member member;
 
-        protected Order() {}
+    protected Order() {
+    }
 
-        public Order(Long id,
-                     Option option,
-                     int quantity,
-                     LocalDateTime orderDateTime,
-                     String message,
-                     Member member) {
-                this.id = id;
-                this.option = option;
-                this.quantity = quantity;
-                this.orderDateTime = orderDateTime;
-                this.message = message;
-                this.member = member;
-        }
+    public Order(Long id,
+                 Option option,
+                 int quantity,
+                 LocalDateTime orderDateTime,
+                 String message,
+                 Member member) {
+        this.id = id;
+        this.option = option;
+        this.quantity = quantity;
+        this.orderDateTime = orderDateTime;
+        this.message = message;
+        this.member = member;
+    }
 
-        public Order(Option option,
-                     int quantity,
-                     String message,
-                     Member member) {
-                this(null, option, quantity, null, message, member);
-        }
+    public Order(Option option,
+                 int quantity,
+                 String message,
+                 Member member) {
+        this(null, option, quantity, null, message, member);
+    }
 
-        @PrePersist
-        protected void onCreate() {
-                orderDateTime = LocalDateTime.now();
-        }
+    @PrePersist
+    protected void onCreate() {
+        orderDateTime = LocalDateTime.now();
+    }
 
-        public Long getId() {return id;}
-        public Option getOption() {return option;}
-        public int getQuantity() {return quantity;}
-        public LocalDateTime getOrderDateTime() {return orderDateTime;}
-        public String getMessage() {return message;}
-        public Member getMember() {return member;}
+    public Long getId() {
+        return id;
+    }
+
+    public Option getOption() {
+        return option;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public LocalDateTime getOrderDateTime() {
+        return orderDateTime;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public Member getMember() {
+        return member;
+    }
 }
