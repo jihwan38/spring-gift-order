4b105536470278d8dc84373c7412ecc763f24915
jihwan38 <sss203800@gmail.com>
2025-07-28 02:53:23 +0900

    feat: OrderRequestDto, OrderResponseDto 구현

--- README.md
+++ README.md
@@ -22,3 +22,5 @@
 ## 1단계 Merge 이후 리뷰 반영
 - [x] .gitignore이 아닌 환경변수 사용하여 RestAPI key 숨기도록 하기
 ## 2단계 - 주문하기
+- [ ] Order API를 위한 기본 틀 구현
+  - [x] OrderRequestDto, OrderResponseDto 구현
---
+++ src/main/java/gift/order/dto/OrderResponseDto.java
@@ -0,0 +1,23 @@
+package gift.order.dto;
+
+import gift.order.entity.Order;
+
+import java.time.LocalDateTime;
+
+public record OrderResponseDto(
+        Long id,
+        Long optionId,
+        int quantity,
+        LocalDateTime orderDateTime,
+        String message
+) {
+    public OrderResponseDto from(Order order) {
+        return new OrderResponseDto(
+                order.getId(),
+                order.getOption().getId(),
+                order.getQuantity(),
+                order.getOrderDateTime(),
+                order.getMessage()
+        );
+    }
+}
